fe1b1adc43e94293b84e26c78145521b488d2292
0rde3pp <pbubna44@gmail.com>
2026-03-29 16:13:16 -0300

    first_proj

---
+++ GenPass.py
@@ -0,0 +1,41 @@
+import random
+import string
+import tkinter as tk
+
+
+janela = tk.Tk()
+janela.title("Gerador de senhas")
+
+def gerar_senha(tamanho_pass):
+    letras = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(letras) for _ in range(tamanho_pass))
+    return password
+
+def genpass():
+    try:
+        tamanho_pass = int(campo.get())
+        password = gerar_senha(tamanho_pass)
+        resultado.config(text=password)
+    except:
+        resultado.config(text="não aceito letra >:| digita NÚMERO RAPA! ")
+
+campo = tk.Entry(janela)
+campo.pack()
+botao = tk.Button(janela, text="Clique em mim", command=genpass)
+resultado = tk.Label(janela, text="")
+botao.pack()
+resultado.pack()
+janela.mainloop()
+
+
+
+
+    #   def genpass(tamanho):
+    #         letras = string.ascii_letters + string.digits + string.punctuation
+    #         password = ''.join(random.choice(letras) for _ in range(tamanho))
+    #         return password
+
+    #     tamanho_pass = int(input("digite o tamanho da senha"))
+    #     pass_gensucess = genpass(tamanho_pass)
+    #     print(f"senha gerada {pass_gensucess}")
+
